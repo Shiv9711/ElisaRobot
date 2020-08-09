aecf905f4d33956fedc8170e09b2d127ddf5d025
Hero Gamer <62762419+Heroofficial@users.noreply.github.com>
2020-08-09 14:43:31 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -20,26 +20,15 @@ from haruka.modules.helper_funcs.misc import paginate_modules
 from haruka.modules.translations.strings import tld, tld_help 
 from haruka.modules.connection import connected
 
-PM_START = """Hello {}, my name is {}!
+PM_START = """Hello {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
-You know how hard it is sometimes to manage group so here is the solution for you
+im a group manager bot maintained by My owner is [Garima](t.me/GarimaQueen).
 
-I'm group manager bot and Anti-spam for RR Players🃏
-
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
-
-My owner is [Aditya 🇮🇳](t.me/xditya)
-
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
+My future updates will be put into This Support Group [ElisaRoBot Group](t.me/Tinder_Queens).
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by [clicking here](http://t.me/Elisa_RoBot?startgroup=true).
 """
 
 
